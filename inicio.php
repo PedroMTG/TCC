ac8212539e7df45da98b3ee8a6210393dcece740
PedroMTG <pedroporfirio2006@gmail.com>
2024-03-21 15:33:06 -0300

    firstcommit

--- inicio.php
+++ inicio.php
@@ -1,1 +1,27 @@
-txt
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo.jpg" alt="" width ="20%">
+        <input type="text" class="barra-pesquisa">
+        <a href="" class="encomendas">Encomendas</a>
+        <a href="">
+            <img src="img/carinho.png" width ="5%" alt="Carrinho">
+        </a>
+    </header>
+    <ul></ul>
+    <main>
+
+    </main>
+
+</body>
+</html>
