e3224a9f850b7b2faba3f60610d917d614b3b3c5
Artorias-8546 <luisporfirio2006@gmail.com>
2024-05-02 15:11:17 -0300

    commitfinal

--- footer.php
+++ footer.php
@@ -2,19 +2,20 @@
     <div class="rodape-corpo font_1">
         <div class="rodape-atendimento font_1">
             <h1>Atendimento</h1>
-            <p>(XX)xxxx-xxxx</p>
-            <p>xxx@ - email</p>
+            <p>15 99607-4470</p>
+            <p>solange.acf@gmail.com</p>
         </div>
 
-        <div class="rodape-redes font_1">
-            <h1>Redes Sociais</h1>
-        </div>
+        <div class="rodape-redes-container">
+            <div class="rodape-redes font_1">
+                <h1>Redes Sociais</h1>
+            </div>
 
-        <div class="rodape-img">
-            <img src="https://www.iconpacks.net/icons/2/free-facebook-icon-2869-thumb.png" alt="https://m.facebook.com/ateliesolangefreitas?locale=ms_MY">
-            <img src="https://cdn-icons-png.flaticon.com/512/739/739244.png" alt="">
+            <div class="rodape-img">
+                <a href="https://www.facebook.com/AmigurumiStoreBrasil/"><img src="https://www.iconpacks.net/icons/2/free-facebook-icon-2869-thumb.png" alt=""></a>
+                <a href="https://www.instagram.com/solange_amigurumi/"><img src="https://cdn-icons-png.flaticon.com/512/739/739244.png" alt=""></a>
+            </div>
         </div>
-
     </div>
 
     <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO   </div>
