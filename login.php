a5b4eb05f84f2f538a5814fe1e16df86c7473910
PedroMTG <pedroporfirio2006@gmail.com>
2024-05-02 15:49:48 -0300

    1.3.1

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <title>Login</title>
+</head>
+<body>
+
+   <main>
+    <div class="login">
+        <div class="login-esquerda">
+            <img src="img/logo.jpg" alt="">
+        </div>
+
+        <div class="login-direita">
+            <h1 class="font_1">Login</h1>
+            <input type="text">
+            <input type="text">
+
+            <div class="acessar">
+                <button class="acessar-botao">Acessar</button>
+            </div>
+
+        </div>
+    </div>
+   </main>
+  
+</body>
+</html>
