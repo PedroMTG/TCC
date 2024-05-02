4c0a5f8eae5e12534318a823dbe9c466d0482783
Artorias-8546 <luisporfirio2006@gmail.com>
2024-05-02 14:32:12 -0300

    foo

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 <footer>
     <div class="rodape-corpo font_1">
+        <div class="rodape-atendimento font_1">
+            <h1>Atendimento</h1>
+            <p>(XX)xxxx-xxxx - whatsapp</p>
+            <p>xxx@ - email</p>
+        </div>
+
+        <div class="rodape-pagamento font_1">
+
+        </div>
 
     </div>
+
+    <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO   </div>
 </footer>
