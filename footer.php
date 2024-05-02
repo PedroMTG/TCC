c0a3b5c70078a08bd6fbcec63060e21601ce7abf
Artorias-8546 <luisporfirio2006@gmail.com>
2024-05-02 15:54:38 -0300

    vasgvas

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
                 <h1>Redes Sociais</h1>
             </div>
 
+            <div class="rodape-linha"> </div>
             <div class="rodape-img">
                 <a href="https://www.facebook.com/AmigurumiStoreBrasil/"><img src="https://www.iconpacks.net/icons/2/free-facebook-icon-2869-thumb.png" alt=""></a>
                 <a href="https://www.instagram.com/solange_amigurumi/"><img src="https://cdn-icons-png.flaticon.com/512/739/739244.png" alt=""></a>
@@ -20,7 +21,8 @@
 
     <div class="rodape-corpo-2">
         <div class="rodape-pagamentos font_f font_1"><p>Formas de pagamento</p></div>
-        <img src="https://logospng.org/wp-content/uploads/pix.png" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg" alt="">
+
     </div>
 
     <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO   </div>
