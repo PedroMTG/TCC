5ab71315fcf6689fd20eb6dfd7f1afe5179e5fb7
Artorias-8546 <luisporfirio2006@gmail.com>
2024-05-06 14:48:16 -0300

    aaa

--- footer.php
+++ footer.php
@@ -23,8 +23,15 @@
     <div class="rodape-corpo-2">
         <div class="rodape-pagamentos font_f font_1"><p>Formas de pagamento</p></div>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png" alt="">
-
     </div>
+
+    <div class="rodape-corpo-3">
+        <div class="rodape-envio font_f font_1"><p>Meios de envio</p></div>
+        <img class="img-rodape-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Correios_%282014%29.svg/1200px-Correios_%282014%29.svg.png" alt="">
+        <img class="img-rodape-2" src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Sedex_logo.png" alt="">
+        <img class="img-rodape-3" src="https://logodownload.org/wp-content/uploads/2017/03/pac-correios-logo.png" alt="">
+    </div>
+
 
     <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO   </div>
 </footer>
