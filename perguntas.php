50c03bce0681b6b0a8e84fdbb835c59820acebd7
PedroMTG <pedroporfirio2006@gmail.com>
2024-05-27 18:25:15 -0300

    5.5

--- perguntas.php
+++ perguntas.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
-    <title>Inicio</title>
+    <script src="script-pergunt.js" defer></script>
+    <script src="script.js" defer></script>
+    <title>Dúvidas Frequentes</title>
 </head>
 <body class="pagina-principal">
 
@@ -42,15 +44,101 @@
         <!-- MAIN - PERGUNTAS -->
     <main>
         <div class="duvidas-cont">
+
+            <!-- PERGUNTA 1 -->
             <div class="perguntas-cont">
                 <div class="duvidas-cont-cima">
-                  <h1 class="pergunt">Pergunta 1</h1>
-                  <img class="triangu-abert" src="img/trianguluFechado.png">
-                  <img class="triangu-fecha" src="img/trianguluAberto.png">
+                  <h1 class="pergunt" id="per1">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt1Visibility()" src="img/trianguluFechado.png" id="tri1">
+                  <img class="triangulo2" onclick="toggleDelt1Visibility()" src="img/trianguluAberto.png" id="tri2">
                 </div>
 
                 <hr class="linha">
-                <p class="respost">Resposta simples e direta ao ponto </p>
+                <p class="respost" id="resp1">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 2 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per2">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt2Visibility()" src="img/trianguluFechado.png" id="tri3">
+                  <img class="triangulo2" onclick="toggleDelt2Visibility()" src="img/trianguluAberto.png" id="tri4">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp2">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 3 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per3">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt3Visibility()" src="img/trianguluFechado.png" id="tri5">
+                  <img class="triangulo2" onclick="toggleDelt3Visibility()" src="img/trianguluAberto.png" id="tri6">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp3">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 4 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per4">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt4Visibility()" src="img/trianguluFechado.png" id="tri7">
+                  <img class="triangulo2" onclick="toggleDelt4Visibility()" src="img/trianguluAberto.png" id="tri8">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp4">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 5 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per5">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt5Visibility()" src="img/trianguluFechado.png" id="tri9">
+                  <img class="triangulo2" onclick="toggleDelt5Visibility()" src="img/trianguluAberto.png" id="tri10">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp5">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 6 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per6">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt6Visibility()" src="img/trianguluFechado.png" id="tri11">
+                  <img class="triangulo2" onclick="toggleDelt6Visibility()" src="img/trianguluAberto.png" id="tri12">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp6">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 7 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per7">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt7Visibility()" src="img/trianguluFechado.png" id="tri13">
+                  <img class="triangulo2" onclick="toggleDelt7Visibility()" src="img/trianguluAberto.png" id="tri14">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp7">Resposta simples e direta ao ponto </p>
+            </div>
+
+            <!-- PERGUNTA 8 -->
+            <div class="perguntas-cont">
+                <div class="duvidas-cont-cima">
+                  <h1 class="pergunt" id="per8">Pergunta 1</h1>
+                  <img class="triangulo1" onclick="toggleDelt8Visibility()" src="img/trianguluFechado.png" id="tri15">
+                  <img class="triangulo2" onclick="toggleDelt8Visibility()" src="img/trianguluAberto.png" id="tri16">
+                </div>
+
+                <hr class="linha">
+                <p class="respost" id="resp8">Resposta simples e direta ao ponto </p>
             </div>
         </div>
     </main>
---
+++ script-pergunt.js
@@ -0,0 +1,150 @@
+function toggleDelt1Visibility() {
+    var t1 = document.getElementById("tri1");
+    var t2 = document.getElementById("tri2");
+    var r1 = document.getElementById("resp1");
+    var p1 = document.getElementById("per1");
+
+    if (t1.style.display === "none") {
+        t1.style.display = "inline";
+        t2.style.display = "none";
+        r1.style.display = "none";
+        p1.style.color = "var(--corfonte)";
+    } else {
+        t1.style.display = "none";
+        t2.style.display = "inline";
+        r1.style.display = "inline";
+        p1.style.color = "gray";
+    }
+}
+
+function toggleDelt2Visibility() {
+    var t3 = document.getElementById("tri3");
+    var t4 = document.getElementById("tri4");
+    var r2 = document.getElementById("resp2");
+    var p2 = document.getElementById("per2");
+    
+    if (t3.style.display === "none") {
+        t3.style.display = "inline";
+        t4.style.display = "none";
+        r2.style.display = "none";
+        p2.style.color = "var(--corfonte)";
+    } else {
+        t3.style.display = "none";
+        t4.style.display = "inline";
+        r2.style.display = "inline";
+        p2.style.color = "gray";
+    }
+}
+
+
+function toggleDelt3Visibility() {
+    var t5 = document.getElementById("tri5");
+    var t6 = document.getElementById("tri6");
+    var r3 = document.getElementById("resp3");
+    var p3 = document.getElementById("per3");
+    
+    if (t5.style.display === "none") {
+        t5.style.display = "inline";
+        t6.style.display = "none";
+        r3.style.display = "none";
+        p3.style.color = "var(--corfonte)";
+    } else {
+        t5.style.display = "none";
+        t6.style.display = "inline";
+        r3.style.display = "inline";
+        p3.style.color = "gray";
+    }
+}
+
+function toggleDelt4Visibility() {
+    var t7 = document.getElementById("tri7");
+    var t8 = document.getElementById("tri8");
+    var r4 = document.getElementById("resp4");
+    var p4 = document.getElementById("per4");
+    
+    if (t7.style.display === "none") {
+        t7.style.display = "inline";
+        t8.style.display = "none";
+        r4.style.display = "none";
+        p4.style.color = "var(--corfonte)";
+    } else {
+        t7.style.display = "none";
+        t8.style.display = "inline";
+        r4.style.display = "inline";
+        p4.style.color = "gray";
+    }
+}
+
+function toggleDelt5Visibility() {
+    var t9 = document.getElementById("tri9");
+    var t10 = document.getElementById("tri10");
+    var r5 = document.getElementById("resp5");
+    var p5 = document.getElementById("per5");
+    
+    if (t9.style.display === "none") {
+        t9.style.display = "inline";
+        t10.style.display = "none";
+        r5.style.display = "none";
+        p5.style.color = "var(--corfonte)";
+    } else {
+        t9.style.display = "none";
+        t10.style.display = "inline";
+        r5.style.display = "inline";
+        p5.style.color = "gray";
+    }
+}
+
+function toggleDelt6Visibility() {
+    var t11 = document.getElementById("tri11");
+    var t12 = document.getElementById("tri12");
+    var r6 = document.getElementById("resp6");
+    var p6 = document.getElementById("per6");
+    
+    if (t11.style.display === "none") {
+        t11.style.display = "inline";
+        t12.style.display = "none";
+        r6.style.display = "none";
+        p6.style.color = "var(--corfonte)";
+    } else {
+        t11.style.display = "none";
+        t12.style.display = "inline";
+        r6.style.display = "inline";
+        p6.style.color = "gray";
+    }
+}
+function toggleDelt7Visibility() {
+    var t13 = document.getElementById("tri13");
+    var t14 = document.getElementById("tri14");
+    var r7 = document.getElementById("resp7");
+    var p7 = document.getElementById("per7");
+    
+    if (t13.style.display === "none") {
+        t13.style.display = "inline";
+        t14.style.display = "none";
+        r7.style.display = "none";
+        p7.style.color = "var(--corfonte)";
+    } else {
+        t13.style.display = "none";
+        t14.style.display = "inline";
+        r7.style.display = "inline";
+        p7.style.color = "gray";
+    }
+}
+function toggleDelt8Visibility() {
+    var t15 = document.getElementById("tri15");
+    var t16 = document.getElementById("tri16");
+    var r8 = document.getElementById("resp8");
+    var p8 = document.getElementById("per8");
+    
+    if (t15.style.display === "none") {
+        t15.style.display = "inline";
+        t16.style.display = "none";
+        r8.style.display = "none";
+        p8.style.color = "var(--corfonte)";
+    } else {
+        t15.style.display = "none";
+        t16.style.display = "inline";
+        r8.style.display = "inline";
+        p8.style.color = "gray";
+    }
+}
