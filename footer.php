b7451929486cdfdd51870566a2cd44248bb71219
Artorias-8546 <luisporfirio2006@gmail.com>
2024-06-05 18:23:15 -0300

    git_c

--- footer.php
+++ footer.php
@@ -1,34 +1,41 @@
 <footer>
 <div class="linha-rodape-1"></div>
-    <div class="rodape-corpo font_1">
-        <div class="rodape-atendimento font_1">
-            <h1>Atendimento</h1>
-            <div class="r-p"><i class="ph ph-phone"></i>15 99607-4470</div>
-            <div class="r-p">solange.acf@gmail.com</div>
+    <div class="rodape-principal">
+        <div class="rodape-corpo font_1">
+            <div class="rodape-atendimento font_1">
+                <div>
+                    <h1>Atendimento</h1>
+                    <div class="r-p"><i class="ph ph-phone"></i>15 99607-4470</div>
+                    <div class="r-p">solange.acf@gmail.com</div>
+                </div>
 
-            <div class="rodape-redes">
-                <h1>Redes Sociais</h1>
+                <div>
+                    <div class="rodape-redes">
+                        <h1>Redes Sociais</h1>
+                    </div>
+
+                    <div class="rodape-img">
+                        <a href="https://www.facebook.com/AmigurumiStoreBrasil/"><img src="https://cdn-icons-png.flaticon.com/256/1051/1051360.png" alt=""></a>
+                        <a href="https://www.instagram.com/solange_amigurumi/"><img src="https://cdn-icons-png.flaticon.com/512/739/739244.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="rodape-segundo">
+            <div class="rodape-corpo-2">
+                <div class="rodape-pagamentos font_f font_1"><p>Formas de pagamento</p></div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png" alt="">
             </div>
 
-            <div class="rodape-img">
-                <a href="https://www.facebook.com/AmigurumiStoreBrasil/"><img src="https://cdn-icons-png.flaticon.com/256/1051/1051360.png" alt=""></a>
-                <a href="https://www.instagram.com/solange_amigurumi/"><img src="https://cdn-icons-png.flaticon.com/512/739/739244.png" alt=""></a>
+            <div class="rodape-corpo-3">
+                <div class="rodape-envio font_f font_1"><p>Meios de envio</p></div>
+                <img class="img-rodape-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Correios_%282014%29.svg/1200px-Correios_%282014%29.svg.png" alt="">
+                <img class="img-rodape-2" src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Sedex_logo.png" alt="">
+                <img class="img-rodape-3" src="https://logodownload.org/wp-content/uploads/2017/03/pac-correios-logo.png" alt="">
             </div>
         </div>
     </div>
-
-    <div class="rodape-corpo-2">
-        <div class="rodape-pagamentos font_f font_1"><p>Formas de pagamento</p></div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png" alt="">
-    </div>
-
-    <div class="rodape-corpo-3">
-        <div class="rodape-envio font_f font_1"><p>Meios de envio</p></div>
-        <img class="img-rodape-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Correios_%282014%29.svg/1200px-Correios_%282014%29.svg.png" alt="">
-        <img class="img-rodape-2" src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Sedex_logo.png" alt="">
-        <img class="img-rodape-3" src="https://logodownload.org/wp-content/uploads/2017/03/pac-correios-logo.png" alt="">
-    </div>
-
     <div class="linha-rodape"></div>
-    <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO   </div>
+    <div class="font_1 rodape-direitos">© 2024. TODOS OS DIREITOS RESERVADOS ME  |  DESENVOLVIDO PELA EQUIPE FIO CRIATIVO</div>
 </footer>
